46d7143bce08febfbcbd2ea969800c10bde2519d
Luis <lvas248@gmail.com>
2022-09-26 17:20:01 -0400

    commit tree fb333167ac32550022696ccad429863890408919

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(array)
+        match_array = []
+         array.each do |word|
+            if word.split('').sort == @word.split('').sort
+                match_array.push(word)
+            end
+        end
+        match_array
+    end
+end
+
+elbow = Anagram.new("elbow")
+elbow.match(["below", "mustard", "this", "sasha"])
+
+# puts ("below".split('').sort == "bowlw".split('').sort)
